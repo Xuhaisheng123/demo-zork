25607469e32f6b538c8d9df214e6a7525eac5ee8
Xuhaisheng123 <18733843342@163.com>
2017-12-12 16:30:27 +0800

    we add more stuffs in the game

--- zork1.py
+++ zork1.py
@@ -9,9 +9,50 @@ def forest():
 			next1 = raw_input(">")
 			if "yes" or "y" in next1:
 				bear_room()
+			else :
+				exit(0)
 		elif "2" in next :
 			print "You are under the way to the most dangerous jounney in the wrold\n You still want go? "
 			next2 = raw_input(">")
+			if "y" or "yes" in next2:
+				tree()
+			else :
+				exit(0)
+			print "You already come down from the tree now,Let's move on "
+			print "You walk in to a area of savages,"
+			print "is very dangerous,you can run away or you can keep going?"
+			next3 = raw_input("'yes' or 'not'>")
+			if next3 == "yes" or "y":
+				print "Congratulations! You are a brave man ,this is the reward for you courage:"
+				print """
+				1.a bow and three arrows
+				2.10kg goldwhich one you want to take?"""
+				next4 = raw_input(">")
+				if next4 == "1":
+					pick_up("bow and arrows")
+				elif next4 =="2":
+					pick_up("gold")
+				else :
+					exit(0)
+				print "Do you want go on or not?"
+				next = raw_input(">")
+				if next == "yes" or "y":
+					print "There is a tiger runs to you"
+					if "bow and arrows" in list:
+						print "You can using the bow to kill the tiger."
+						next = raw_input("input 'y' or 'n'>")
+						if next == "y":
+							print "you kill the tiger the reward is 1000kg gold"
+							print "you are win!"
+						else :
+							dead("The tiger kills you,you are dead.")
+					else :
+						dead("You have nothing to fight the tiger you are dead.")
+				else :
+					exit(0)
+			if next3 == "no" or "not" or "n":
+				exit(0)
+				
 		else:
 			print "You need to input the right path,such as 1 or 2."
 			
@@ -38,7 +79,7 @@ def bear_room():
 			print "All the things you have now:%s"%list
 		elif "2" in next1:
 			dead("The bear kills you.")
-		else :
+		elif "3" in next1:
 			print "You are safe now!"
 			print "All the things you have now:%s"%list
 			print "You want drop the sward or not?"
@@ -46,7 +87,10 @@ def bear_room():
 			if input == "yes" or "y":
 				drop("sward")
 				print list
-
+		else:
+			exit(0)
+	else:
+		exit(0)
 def dead(why):
 		print why,"byebye!"
 		
@@ -57,6 +101,7 @@ def dead(why):
 			exit(0)	
 
 def pick_up(stuff):
+	print "The %s is yours now!"%stuff
 	flag = True
 	if stuff in list:
 		#if list[i]==stuff:
@@ -73,4 +118,14 @@ def drop(stuff):
 		
 	if flag == True:
 		list.remove(stuff)
+def tree():
+	print "There is a ganit tree  beside the road.\nDo you want climbing it?"
+	next0 = raw_input(">")
+	if "yes" or "y" or "climbing" in next0:	
+		print "You are 30 meters from the ground now!"
+		print "There are some eggs in the nest,you want take it?"
+		next1 = raw_input(">")
+		if "yes" or "y" or "taken" in next1:
+			pick_up("eggs")
+			print "That's what you have now:%s"%list
 forest()
